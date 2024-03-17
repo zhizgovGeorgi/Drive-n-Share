a3ad8e1469ff8d21b730e51cf24136989bd06383
Georgi Zhizgov <i463263@fhict.nl>
2024-03-17 08:41:28 +0100

    Reinitialize with Maven

---
+++ travel-service/src/test/java/com/example/travelservice/TravelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.travelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
