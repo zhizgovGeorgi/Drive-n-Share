03681eb4aea3352eaea1d026bc2d54a11d3e35de
Georgi Zhizgov <i463263@fhict.nl>
2024-05-21 17:45:50 +0200

    full switch to sonarcloud from qodana

--- api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/example/apigateway/config/SecurityConfig.java
@@ -1,2 +1,14 @@
-package com.example.apigateway.config;public class SecurityConfig {
+package com.example.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class SecurityConfig {
+//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+//        return httpSecurity.authorizeHttpRequests(authorize->authorize.anyRequest()
+//                .authenticated())
+//                .oauth2ResourceServer(oauth2 ->oauth2.jwt(Customizer.withDefaults()))
+//                .build();
+//    }
 }
